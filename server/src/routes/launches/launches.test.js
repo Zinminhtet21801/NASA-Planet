b421d29edd3ebd2327454ee3f9cd92fa1d63a30a
Zin Min Htet <zetminhtin@gmail.com>
2022-03-20 11:31:11 +0630

    removing console log after killing process

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,9 +4,17 @@ const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 const { loadPlanetsData } = require("../../models/planets.model");
 
 describe("Launches API", () => {
-  beforeAll(async () => {
-    await mongoConnect();
-    await loadPlanetsData();
+  // beforeAll(async () => {
+  //   await mongoConnect();
+  //   await loadPlanetsData();
+  // });
+
+  beforeAll(() => {
+    return new Promise((resolve) => {
+      mongoConnect();
+      loadPlanetsData();
+      resolve();
+    });
   });
 
   // afterAll(async () => {
